29d7325a304436b57bb1ce4155af6c9c928cd714
williamsna <nathan.williams1@booking.com>
2025-01-13 13:06:50 +0000

    Completed 09.

--- 09. rotate-angle/solution.js
+++ 09. rotate-angle/solution.js
@@ -1,8 +1,24 @@
 function rotateAngleByDegrees(initialAngle, rotation) {
   // Calculate the new angle
   let newAngle = initialAngle + (rotation % 360);
-
-  return newAngle;
+  // let newAngle = initialAngle + rotation;
+  if (newAngle === 360) {
+    return 0;
+  }
+  else if(newAngle === 0) {
+    return initialAngle;
+  }
+  else if (newAngle > 360) {
+    return newAngle - 360;
+  }
+  else if (newAngle < 0) {
+  return newAngle + 360;
+  }
+  else {
+  return newAngle}
 }
+
+console.log(rotateAngleByDegrees(359,1));
+
 
 module.exports = { rotateAngleByDegrees };
