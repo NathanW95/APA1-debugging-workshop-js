06b5099965171fc98268272b6488cece7401da0e
williamsna <nathan.williams1@booking.com>
2025-01-13 12:36:15 +0000

    Completed 06.

--- 06. 12-hour-clock/solution.js
+++ 06. 12-hour-clock/solution.js
@@ -3,7 +3,17 @@ function convertTo12HourClock(time) {
   const minutes = time.slice(-2);
   if (hours > 12) {
     return `${hours - 12}:${minutes} PM`;
-  } else {
+  }
+  else if (hours === "00") {
+    return `12:${minutes} AM`;
+  }
+  else if (hours === "12") {
+    return `12:${minutes} PM`;
+  }
+  else if (hours < 10) {
+    return `${hours[1]}:${minutes} AM`;
+  }
+  else {
     return `${hours}:${minutes} AM`;
   }
 }
