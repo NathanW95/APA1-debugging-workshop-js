8f9a6e73a2d377b92bad7ea93c99cfa60c468971
williamsna <nathan.williams1@booking.com>
2025-01-13 12:24:05 +0000

    Completed 04.

--- 04. percentage-change/solution.js
+++ 04. percentage-change/solution.js
@@ -1,8 +1,17 @@
 function calculatePercentageChange(original, newAmount) {
   const difference = Math.abs(newAmount - original);
-  const average = (original - newAmount) / 2;
-  const percentageDifference = (average / difference) * 100;
-  return percentageDifference.toFixed(2);
+  const percentageDifference = (difference / original) * 100;
+
+  if (difference === 0) {
+    return 0;
+  }
+  else if (original > newAmount) {
+    return Math.ceil(percentageDifference *-1);
+  }
+  else {
+    return percentageDifference;
+  }
 }
 
+calculatePercentageChange(50, 40);
 module.exports = { calculatePercentageChange };
